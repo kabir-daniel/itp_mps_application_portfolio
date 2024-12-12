519cd8089caa38d7670733bed78eed79dceebe44
kabir-daniel <kabirrdaniel@gmail.com>
2024-12-11 16:02:53 -0800

    Quartz sync: Dec 11, 2024, 4:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Amaranth",
+        body: "Overlock",
         code: "IBM Plex Mono",
       },
       colors: {
